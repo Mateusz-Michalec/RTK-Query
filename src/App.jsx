2d038e8cd8440523e8c344bfb2f17434e7251ab5
MatthewDevv <117437977+MatthewWDFD@users.noreply.github.com>
2023-07-03 09:55:09 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import React from "react";
+import TodoList from "./features/todos/TodoList";
+
+export default function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
